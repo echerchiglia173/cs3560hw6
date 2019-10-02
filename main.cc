292a124ab2a3f2aa14d2ce5de171806484b4eef9
echerchiglia173 <e.cherchiglia@gmail.com>
2019-10-02 16:21:56 -0400

    commenting main.cc

--- main.cc
+++ main.cc
@@ -2,15 +2,19 @@
 // CS2401
 // November 23, 2015
 // Project7
-
+/// includes game.h
 #include "game.h"
+///includes othello.h
 #include "othello.h"
 using namespace main_savitch_14;
 
-
+/// this is the main program
 int main()
 {
+/// This command will run the game
 	Othello theGame;
+	///This command will restart the game using the game
 	theGame.restart();
+	///this command with play the game
 	theGame.play();
 }
